0b957d5c3207d858cf1efd472913b3e45eab57c8
Wenjun Mao <mkof616@gmail.com>
2023-06-26 18:52:03 -0400

    Add a test-service to Docker Compose

--- web/src/page_tracker/app.py
+++ web/src/page_tracker/app.py
@@ -1,6 +1,7 @@
 # src/page_tracker/app.py
 
 import os
+import time
 from functools import cache
 
 from flask import Flask
@@ -12,7 +13,10 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     try:
+        start = time.time()
         page_views = redis().incr("page_views")
+        end = time.time()
+        app.logger.info("Redis operation took %s seconds.", end - start)
     except RedisError:
         app.logger.exception("Redis error")
         return "Sorry, something went wrong \N{pensive face}", 500

a6db1ba123e348bdffa6ca05111563e3b7c52c2a
Wenjun Mao <mkof616@gmail.com>
2023-06-27 16:06:15 -0400

    Replace the emoji in an error message

--- web/src/page_tracker/app.py
+++ web/src/page_tracker/app.py
@@ -19,7 +19,7 @@ def index():
         app.logger.info("Redis operation took %s seconds.", end - start)
     except RedisError:
         app.logger.exception("Redis error")
-        return "Sorry, something went wrong \N{pensive face}", 500
+        return "Sorry, something went wrong \N{thinking face}", 500
     else:
         return f"This page has been seen {page_views} times."
 
